b04db16473b4232ee39e322d062f1b37b692881d
Jihad Noureddine <jihad.nd@gmail.com>
2021-04-23 03:14:58 +0300

    JWT is set up

--- app/User.php
+++ app/User.php
@@ -1,46 +1,59 @@
 <?php
-
 namespace App;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
-    use Notifiable;
+  use Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'mobile_number', 'password',"opt",
-    ];
+  /**
+   * The attributes that are mass assignable.
+   *
+   * @var array
+   */
+  protected $fillable = [
+      'name', 'email', 'password',
+  ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
+  /**
+   * The attributes that should be hidden for arrays.
+   *
+   * @var array
+   */
+  protected $hidden = [
+      'password', 'remember_token',
+  ];
 
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+  /**
+   * The attributes that should be cast to native types.
+   *
+   * @var array
+   */
+  protected $casts = [
+      'email_verified_at' => 'datetime',
+  ];
 
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+    public function setPasswordAttribute($password)
+    {
+        if ( !empty($password) ) {
+            $this->attributes['password'] = bcrypt($password);
+        }
+    }    
+    
     public function plan()
     {
         return $this->belongsTo('App\Plan');
     }
-
-    
-}
+  }
--- routes/api.php
+++ routes/api.php
@@ -3,45 +3,15 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
-Route::get("/send",function(){
-    $basic  = new \Vonage\Client\Credentials\Basic("4ef82e07", "EpPOgXeeax3AYyoR");
-    $client = new \Vonage\Client($basic);
-    $response = $client->sms()->send(
-        new \Vonage\SMS\Message\SMS("96176452125", "JND", 'A text message sent using the Nexmo SMS API')
-    );
-    
-    $message = $response->current();
-    
-    if ($message->getStatus() == 0) {
-        echo "The message was sent successfully\n";
-    } else {
-        echo "The message failed with status: " . $message->getStatus() . "\n";
-    }
-});
 
 //////////Auth APIs//////////
-Route::post("/login","UserController@login");
-Route::post("/verify-otp","UserController@verifyOtp");
-Route::post("/signup","UserController@signup");
-Route::post("/logout","UserController@logout");
+Route::post("/verify-otp","AuthController@verifyOtp");
 
-////////Packages API/////////
-//TODO JWT MIDDLEWARE
-Route::post("/packages","PackagesController@getAllPackages");
-   
- 
+Route::post('/signup', 'AuthController@register');
+Route::post('/login', 'AuthController@login');
+Route::post('/logout', 'AuthController@logout');
+
+////////Packages API (jwt🔒) ///////// 
+Route::group(['middleware' => ['jwt.verify']], function() {
+    Route::post("/packages","PackagesController@getAllPackages");
+});
