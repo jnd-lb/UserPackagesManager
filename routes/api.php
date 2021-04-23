f8dc729b17d883ae1606b35ee57b5b04fece02d8
Jihad Noureddine <jihad.nd@gmail.com>
2021-04-23 03:26:35 +0300

    JWT is set up

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,22 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
+Route::get("/send",function(){
+    $basic  = new \Vonage\Client\Credentials\Basic("4ef82e07", "EpPOgXeeax3AYyoR");
+    $client = new \Vonage\Client($basic);
+    $response = $client->sms()->send(
+        new \Vonage\SMS\Message\SMS("96176452125", "JND", 'A text message sent using the Nexmo SMS API')
+    );
+    
+    $message = $response->current();
+    
+    if ($message->getStatus() == 0) {
+        echo "The message was sent successfully\n";
+    } else {
+        echo "The message failed with status: " . $message->getStatus() . "\n";
+    }
+});
+
 //////////Auth APIs//////////
 Route::post("/verify-otp","AuthController@verifyOtp");
 
